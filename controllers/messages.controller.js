fa1e9f0c8ca4c30fc37f9a1d448febf13bac5179
Andrew Talle <talle.andrew@gmail.com>
2021-06-03 15:02:54 -0600

    seperate different templates

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,13 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(
-    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
-  );
+  // res.sendFile(
+  //   path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  // );
+  res.render("messages", {
+    title: "Messages to my friends",
+    friend: "Amanda Talle",
+  });
 }
 
 function postMessage(req, res) {
