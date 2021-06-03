74f9086c44d74852a346d9621eb0e02fcbc11290
Andrew Talle <talle.andrew@gmail.com>
2021-06-03 14:39:33 -0600

    add static files via middleware

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require("path");
+
 function getMessages(req, res) {
-  res.send("<ul><li>Hello Andrew!</li></ul>");
+  res.sendFile(
+    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  );
 }
 
 function postMessage(req, res) {
